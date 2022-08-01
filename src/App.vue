dd95bfc6952b2974fd96a58b3de16dc936c4ba9f
jasonjsyuan <jasonjsyuan@tencent.com>
2022-08-01 17:34:31 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <FileList></FileList>
+  </div>
+</template>
+
+<script>
+import FileList from './components/FileList'
+
+export default {
+  name: 'App',
+  components: {
+    FileList
+  }
+}
+</script>
+
+<style>
+.body {
+  overflow-x: hidden;
+}
+</style>
