2d335d812da7814c481e3649cf7c0fdcafe819a4
jasonjsyuan <jasonjsyuan@tencent.com>
2024-05-08 20:22:46 +0800

    show set bits in list

--- src/components/BitArrayTag.vue
+++ src/components/BitArrayTag.vue
@@ -1,35 +1,43 @@
 <script setup lang="ts">
-import { computed } from '@vue/reactivity';
-import Tag from './Tag.vue'
+import { computed } from "@vue/reactivity";
+import Tag from "./Tag.vue";
 
 const props = defineProps({
-    value: {
-        type: Number,
-        default: 0,
-    },
-    descriptions: {
-        type: Array<String>,
-        default: [],
-    }
+  value: {
+    type: String,
+    default: "",
+  },
+  descriptions: {
+    type: Array<String>,
+    default: [],
+  },
 });
 
 const tags = computed(() => {
-    const result = [];
-    let mask = 1;
-    for (let i = 0; i < props.descriptions.length; i++) {
-        const desc = props.descriptions[i];
-        result.push({ name: desc, exist: (mask & props.value) == mask });
-        mask <<= 1;
-    }
-    return result;
-})
+  const result = [];
+  let mask: BigInt = 1n;
+  let value: BigInt = BigInt(props.value);
+  for (let i = 0; i < props.descriptions.length; i++) {
+    const desc = props.descriptions[i];
+    result.push({
+      name: desc,
+      exist: (mask.valueOf() & value.valueOf()) === mask,
+    });
+    mask = mask.valueOf() << 1n;
+  }
+  return result;
+});
 </script>
 
 <template>
-    <div class="bitarraytag">
-        <Tag v-for="tag in tags" :key="(tag.name as string)" :disabled="!tag.exist">{{ tag.name }}</Tag>
-    </div>
+  <div class="bitarraytag">
+    <Tag
+      v-for="tag in tags"
+      :key="(tag.name as string)"
+      :disabled="!tag.exist"
+      >{{ tag.name }}</Tag
+    >
+  </div>
 </template>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
-import BitArrayTag from './BitArrayTag.vue'
+import { onMounted, ref } from "vue";
+import BitArrayTag from "./BitArrayTag.vue";
 
-const value = ref(0);
+const value = ref(0n);
 
 const props = defineProps({
   descriptions: {
     type: Array<String>,
-  }
+  },
 });
 
 onMounted(() => {
@@ -15,23 +15,43 @@ onMounted(() => {
   window.utools?.onPluginEnter(() => {
     // @ts-ignore
     window.utools?.setSubInput(({ text }) => {
-      inputChange(text)
-    }, '输入当前值')
+      inputChange(text);
+    }, "输入当前值");
   });
 });
 
+const findSetBits = function (text: string) {
+  const number = BigInt(text);
+  const binaryString = number.toString(2);
+  const setBits = [];
+  for (let i = binaryString.length - 1; i >= 0; i--) {
+    if (binaryString[i] === "1") {
+      setBits.push(binaryString.length - i);
+    }
+  }
+  return setBits;
+};
+
+const bitList = ref(findSetBits(value.value.toString()));
 const inputChange = function (text: string) {
-  value.value = Number.parseInt(text);
-}
+  value.value = BigInt(text);
+  bitList.value = findSetBits(text);
+};
 </script>
 
 <template>
   <div>
     <div class="display">
-      <BitArrayTag :value="value" :descriptions="descriptions" />
+      <BitArrayTag :value="value.toString()" :descriptions="descriptions" />
+      <div id="bit-list">被设置的位：{{ bitList }}</div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+#bit-list {
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+}
 </style>
